185f7b07c912ff0a4b714f4380673d1e9afe7901
johnjubes <john.n.jubes@gmail.com>
2025-08-10 15:21:50 -0600

    change payload, add conditional on push notifications, add update notification

---
+++ .github/scripts/notify-update.py
@@ -0,0 +1,31 @@
+import boto3
+import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import messaging
+
+bucket_name = "wicked-woods-notifications"
+creds_file = "ome-push-notifications-firebase-adminsdk-fbsvc-e58bb1d562.json"
+
+def get_creds_file():
+    s3_client = boto3.client("s3")
+    s3_client.download_file(bucket_name, creds_file, "creds.json")
+
+def send_notification():
+    
+    cred = credentials.Certificate("creds.json")
+    firebase_admin.initialize_app(cred)
+    message = messaging.Message(
+        data={
+            'updated': "true",
+        },
+        topic="wicked-woods_2025_general",
+    )
+    
+    # send message
+    response = messaging.send(message)
+                      
+
+get_creds_file()
+send_notification()
+print("script has run")
